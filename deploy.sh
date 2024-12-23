26d29f5b325d403e06f137e5d0c12cc6fb933fc7
Theodore Keloglou <zf@sirodoht.com>
2024-12-23 22:29:39 +0000

    Fix deploy script shebang

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-#!/usr/local/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 ssh root@brick.01z.io 'sudo -i -u deploy bash -c "cd /var/www/sirodoht && git pull"'
 scp sirodoht.caddy root@brick.01z.io:/etc/caddy/sirodoht.caddy
 ssh root@brick.01z.io 'systemctl restart caddy'
-
