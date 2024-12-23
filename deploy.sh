18fe62a00333f3b8c00bb999e513075a32c5c115
Theodore Keloglou <zf@sirodoht.com>
2024-12-23 22:26:07 +0000

    Change deploy to new server

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-ssh root@sirodoht.com 'sudo -i -u deploy bash -c "cd /var/www/sirodoht && git pull"'
+#!/usr/local/bin/bash
+
+ssh root@brick.01z.io 'sudo -i -u deploy bash -c "cd /var/www/sirodoht && git pull"'
+scp sirodoht.caddy root@brick.01z.io:/etc/caddy/sirodoht.caddy
+ssh root@brick.01z.io 'systemctl restart caddy'
+
